dde0ad2a411798acb55f852d1215ea316b513496
akiyoshi <pasoconnfast@gmail.com>
2025-10-11 20:16:26 +0900

    Delete api directory

--- api/admin/tickets/index.js
+++
@@ -1,7 +0,0 @@
-export default function handler(req, res) {
-  if (req.method !== 'GET') return res.status(405).end();
-
-  const tickets = global.tickets || [];
-  const simplified = tickets.map(({ expiryTime, ...rest }) => rest);
-  res.status(200).json(simplified);
-}
